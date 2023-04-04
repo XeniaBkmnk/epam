d63549ba9ebbd34525580089ad2ae5954afec0fe
XeniaBkmnk <128148178+XeniaBkmnk@users.noreply.github.com>
2023-04-04 23:30:47 +0200

    Add files via upload

---
+++ Task7.java
@@ -0,0 +1,21 @@
+package Task2;
+
+import java.util.Scanner;
+
+public
+    class Task7 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print
+                (
+                        "Введите целое положительное число: "
+                );
+        int n = input.nextInt();
+        int sum = 0;
+        for (int i = 1; i <= n; i++) {
+            sum += i;
+        }
+        System.out.println("Сумма чисел от 1 до " + n + " равна " + sum);
+
+    }
+}
