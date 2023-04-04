f0f699393ed388b4bf1ce5dc804f9878d564e548
XeniaBkmnk <128148178+XeniaBkmnk@users.noreply.github.com>
2023-04-05 00:13:37 +0200

    Add files via upload

---
+++ Task10.java
@@ -0,0 +1,27 @@
+package Task2;
+
+import java.util.Scanner;
+
+public
+    class Task10 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Ввeдите m: ");
+        int m = scanner.nextInt();
+
+        System.out.println("Введите n: ");
+        int n = scanner.nextInt();
+
+        for (int i = m; i <= n; i++) {
+            System.out.println("Длинна числа " + i + ": ");
+            for (int j = 2; j < i; j++) {
+                if (i % j == 0) {
+                    System.out.print(j + " ");
+                }
+            }
+            System.out.println();
+        }
+        scanner.close(); //зарываем сканер
+    }
+}
