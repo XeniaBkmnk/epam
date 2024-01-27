0c2e57274b92dd90fff17c26daf46beaeae9fcc5
XeniaBkmnk <128148178+XeniaBkmnk@users.noreply.github.com>
2024-01-27 23:58:33 +0300

    Update Task2.java

--- Task2.java
+++ Task2.java
@@ -3,13 +3,10 @@ package Task2;
 public
     class Task2 {
     public static void main(String[] args) {
-
         double b , a, c;
         b=11;
         a=11;
         c=1;
-
-
         double Answer = b + Math.sqrt(Math.pow(b, 2) + 4 * a * c) / 2 * a - (Math.pow(a, 3)) * c + (Math.pow(b, -2));
         System.out.println("Task 2");
         System.out.println((int)Answer);
